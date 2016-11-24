3d31b1a3c4f2534655ef2acdf92119ec6532a101
Mislav Stankovic <mislav.stankovic@gmail.com>
2016-11-24 20:51:03 +0100

    Add initial Gradle project files

---
+++ src/main/java/hr/mistankovic/springbuilddemo/SpringBuildDemoApplication.java
@@ -0,0 +1,12 @@
+package hr.mistankovic.springbuilddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuildDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuildDemoApplication.class, args);
+	}
+}
